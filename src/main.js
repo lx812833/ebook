6d7872b4bbe51c2840b6d07413db310cb35e5e0d
lx812833 <lx812833@163.com>
2019-01-18 21:53:43 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -7,10 +7,14 @@ import store from './store'
 import './assets/styles/icon.css'
 // 导入全局global样式
 import './assets/styles/global.scss'
+// 引入多语言
+import i18n from './lang'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
